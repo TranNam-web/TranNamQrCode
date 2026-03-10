2d37f8dccbee1f3e23eb18039744f434e20dd9b9
TranNam-web <namduchuong204@gmail.com>
2026-03-10 23:08:48 +0700

    fix sitemap build vercel

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,7 @@ import dishApiRequest from '@/apiRequests/dish'
 import envConfig, { locales } from '@/config'
 import { generateSlugUrl } from '@/lib/utils'
 import type { MetadataRoute } from 'next'
+
 export const dynamic = 'force-dynamic'
 
 const staticRoutes: MetadataRoute.Sitemap = [
@@ -18,34 +19,40 @@ const staticRoutes: MetadataRoute.Sitemap = [
 ]
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const result = await dishApiRequest.list()
+  let dishList: any[] = []
 
-  const dishList = result.payload.data
+  try {
+    const result = await dishApiRequest.list()
+    dishList = result?.payload?.data || []
+  } catch (error) {
+    console.log('Sitemap API error:', error)
+    dishList = []
+  }
+
   const localizeStaticSiteMap = locales.reduce((acc, locale) => {
     return [
       ...acc,
-      ...staticRoutes.map((route) => {
-        return {
-          ...route,
-          url: `${envConfig.NEXT_PUBLIC_URL}/${locale}${route.url}`,
-          lastModified: new Date()
-        }
-      })
+      ...staticRoutes.map((route) => ({
+        ...route,
+        url: `${envConfig.NEXT_PUBLIC_URL}/${locale}${route.url}`,
+        lastModified: new Date()
+      }))
     ]
   }, [] as MetadataRoute.Sitemap)
+
   const localizeDishSiteMap = locales.reduce((acc, locale) => {
-    const dishListSiteMap: MetadataRoute.Sitemap = dishList.map((dish) => {
-      return {
-        url: `${envConfig.NEXT_PUBLIC_URL}/${locale}/dishes/${generateSlugUrl({
-          id: dish.id,
-          name: dish.name
-        })}`,
-        lastModified: dish.updatedAt,
-        changeFrequency: 'weekly',
-        priority: 0.9
-      }
-    })
+    const dishListSiteMap: MetadataRoute.Sitemap = dishList.map((dish) => ({
+      url: `${envConfig.NEXT_PUBLIC_URL}/${locale}/dishes/${generateSlugUrl({
+        id: dish.id,
+        name: dish.name
+      })}`,
+      lastModified: dish.updatedAt || new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9
+    }))
+
     return [...acc, ...dishListSiteMap]
   }, [] as MetadataRoute.Sitemap)
+
   return [...localizeStaticSiteMap, ...localizeDishSiteMap]
 }
