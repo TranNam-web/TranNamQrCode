7c8ac0735336f3cf77d68d2b635866d4e5d472b2
TranNam-web <namduchuong204@gmail.com>
2026-03-11 23:38:51 +0700

    fix env api endpoint

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import dishApiRequest from '@/apiRequests/dish'
 import envConfig, { locales } from '@/config'
 import { generateSlugUrl } from '@/lib/utils'
 import type { MetadataRoute } from 'next'
-export const dynamic = 'force-dynamic'
+
 const staticRoutes: MetadataRoute.Sitemap = [
   {
     url: '',
@@ -17,43 +17,34 @@ const staticRoutes: MetadataRoute.Sitemap = [
 ]
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const result = await dishApiRequest.list()
 
-  let dishList: any[] = []
-
-  // ❗ chỉ gọi API khi không phải build
-  if (process.env.VERCEL_ENV !== 'production') {
-    try {
-      const result = await dishApiRequest.list()
-      dishList = result?.payload?.data || []
-    } catch {
-      dishList = []
-    }
-  }
-
+  const dishList = result.payload.data
   const localizeStaticSiteMap = locales.reduce((acc, locale) => {
     return [
       ...acc,
-      ...staticRoutes.map((route) => ({
-        ...route,
-        url: `${envConfig.NEXT_PUBLIC_URL}/${locale}${route.url}`,
-        lastModified: new Date()
-      }))
+      ...staticRoutes.map((route) => {
+        return {
+          ...route,
+          url: `${envConfig.NEXT_PUBLIC_URL}/${locale}${route.url}`,
+          lastModified: new Date()
+        }
+      })
     ]
   }, [] as MetadataRoute.Sitemap)
-
   const localizeDishSiteMap = locales.reduce((acc, locale) => {
-    const dishListSiteMap: MetadataRoute.Sitemap = dishList.map((dish) => ({
-      url: `${envConfig.NEXT_PUBLIC_URL}/${locale}/dishes/${generateSlugUrl({
-        id: dish.id,
-        name: dish.name
-      })}`,
-      lastModified: dish.updatedAt,
-      changeFrequency: 'weekly',
-      priority: 0.9
-    }))
-
+    const dishListSiteMap: MetadataRoute.Sitemap = dishList.map((dish) => {
+      return {
+        url: `${envConfig.NEXT_PUBLIC_URL}/${locale}/dishes/${generateSlugUrl({
+          id: dish.id,
+          name: dish.name
+        })}`,
+        lastModified: dish.updatedAt,
+        changeFrequency: 'weekly',
+        priority: 0.9
+      }
+    })
     return [...acc, ...dishListSiteMap]
   }, [] as MetadataRoute.Sitemap)
-
   return [...localizeStaticSiteMap, ...localizeDishSiteMap]
 }
